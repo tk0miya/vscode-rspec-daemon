b41e617263e8c9373ff8af8d52fae812f84a9c09
Takeshi KOMIYA <i.tkomiya@gmail.com>
2023-05-21 13:16:38 +0900

    test: Update example

--- test/spec/example_spec.rb
+++ test/spec/example_spec.rb
@@ -2,8 +2,24 @@
 
 require 'rspec'
 
-RSpec.describe 'rspec-daemon example' do
-  subject { 'rspec-daemon' }
+load 'app/example.rb'
 
-  it { is_expected.to be_kind_of String }
+RSpec.describe 'rspec-daemon example' do
+  describe 'sum' do
+    subject { sum(x, y) }
+
+    context 'When Integer given' do
+      let(:x) { 1 }
+      let(:y) { 2 }
+
+      it { is_expected.to eq 3 }
+    end
+
+    context 'When String given' do
+      let(:x) { 'Hello ' }
+      let(:y) { 'World' }
+
+      it { is_expected.to eq 'Hello World' }
+    end
+  end
 end
